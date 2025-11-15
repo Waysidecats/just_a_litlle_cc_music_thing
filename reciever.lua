1d320dcc1076fa47b6c9117242f65c28c55d77e0
Waysidecats <103798521+Waysidecats@users.noreply.github.com>
2025-11-14 16:44:29 -0800

    Update reciever.lua

--- reciever.lua
+++ reciever.lua
@@ -1,82 +1,58 @@
--- startup.lua (receiver)
+-- receiver_startup.lua
 
 local dfpwm = require("cc.audio.dfpwm")
 local decoder = dfpwm.make_decoder()
 local speakers = { peripheral.find("speaker") }
-local rednetChannel = 16783
 
--- Find modem the clean way
+local volume = settings.get("media_center.volume") or 1.0
 local modem = peripheral.find("modem")
-if not modem then error("Receiver requires a modem!") end
 rednet.open(peripheral.getName(modem))
 
--- Tick counter
-local currentTick = 0
-local function tickCounter()
-    while true do
-        os.pullEvent("tick")
-        currentTick = currentTick + 1
-    end
-end
+local PROTOCOL = "monolith-local-radio"
+
+local globalTick = 0
+local buffer = {}
 
 local function playChunk(chunk)
-    local retval = nil
-    local calls = {}
+    local ok
+    local tasks = {}
 
     for i, sp in ipairs(speakers) do
         if i == 1 then
-            table.insert(calls, function() retval = sp.playAudio(chunk, 1.0) end)
+            table.insert(tasks, function()
+                ok = sp.playAudio(chunk, volume)
+            end)
         else
-            table.insert(calls, function() sp.playAudio(chunk, 1.0) end)
+            table.insert(tasks, function()
+                sp.playAudio(chunk, volume)
+            end)
         end
     end
 
-    parallel.waitForAll(table.unpack(calls))
-    return retval
+    parallel.waitForAll(table.unpack(tasks))
+    return ok
 end
 
-print("Receiver online. Waiting for broadcast...")
-
 while true do
-    local id, msg = rednet.receive(rednetChannel)
+    local id, msg = rednet.receive(PROTOCOL, 0.05)
 
-    if msg.stop then
-        print("Received STOP command. Rebooting.")
-        sleep(0.5)
-        os.reboot()
+    -- clock ticks
+    if msg and msg.type == "clock" then
+        globalTick = msg.tick
     end
 
-    if msg.url and msg.startTick then
-        local uri = msg.url
-        local startTick = msg.startTick
+    -- audio chunks
+    if msg and msg.type == "audio" then
+        buffer[msg.play_at] = msg.chunk
+    end
 
-        print("Received sync start:", uri, startTick)
+    -- If it's time to play
+    local chunk = buffer[globalTick]
+    if chunk then
+        buffer[globalTick] = nil
 
-        parallel.waitForAny(
-            tickCounter,
-            function()
-                while currentTick < startTick do
-                    os.pullEvent("tick")
-                end
-
-                print("Starting audio at:", currentTick)
-
-                local response = http.get(uri, nil, true)
-                local chunkSize = 4 * 1024
-
-                while true do
-                    local chunk = response.read(chunkSize)
-                    if not chunk then break end
-
-                    local buffer = decoder(chunk)
-
-                    while not playChunk(buffer) do
-                        os.pullEvent("speaker_audio_empty")
-                    end
-                end
-
-                print("Stream ended. Waiting for next broadcast.")
-            end
-        )
+        while not playChunk(chunk) do
+            os.pullEvent("speaker_audio_empty")
+        end
     end
 end
