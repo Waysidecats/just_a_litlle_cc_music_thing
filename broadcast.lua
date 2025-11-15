1d0562bf327639cca2660bcb909f763a3c0bfb0a
Waysidecats <103798521+Waysidecats@users.noreply.github.com>
2025-11-14 17:00:14 -0800

    Update broadcast.lua

--- broadcast.lua
+++ broadcast.lua
@@ -1,5 +1,3 @@
--- stream_sync_host.lua
-
 local dfpwm = require("cc.audio.dfpwm")
 local decoder = dfpwm.make_decoder()
 
@@ -8,20 +6,76 @@ rednet.open(peripheral.getName(modem))
 
 local PROTOCOL = "monolith-local-radio"
 
-local uri = "<YOUR STREAM URL HERE>"
+local speakers = { peripheral.find("speaker") } -- not used for network streaming, but kept for reference
 
+-- --- SONG SELECTION LOGIC ---
+local fs = fs
+local menu = require "menu"
+local uri = nil
+
+local drive = peripheral.find("drive")
+local selectedSong = nil
+
+-- choose song from local files if no disk is present
+if drive == nil or not drive.isDiskPresent() then
+    local savedSongs = fs.list("songs/")
+
+    if #savedSongs == 0 then
+        error("ERR - No disk found and no local songs available.")
+    else
+        local entries = {
+            [1] = { label = "[CANCEL]", callback = function() error() end }
+        }
+
+        for i, fp in ipairs(savedSongs) do
+            table.insert(entries, {
+                label = fp:match("^([^.]+)"),
+                callback = function()
+                    selectedSong = fp
+                    menu.exit()
+                end
+            })
+        end
+
+        menu.init({ main = { entries = entries } })
+        menu.thread()
+
+        if selectedSong then
+            local fp = "songs/" .. selectedSong
+            if fs.exists(fp) then
+                local file = fs.open(fp, "r")
+                uri = file.readAll()
+                file.close()
+            else
+                error("Song file not found on device!")
+            end
+        else
+            error("No song selected!")
+        end
+    end
+else
+    -- disk present, read song.txt
+    local songFile = fs.open("disk/song.txt", "r")
+    uri = songFile.readAll()
+    songFile.close()
+end
+
+-- sanity check
+if not uri or not uri:find("^https") then
+    error("ERR - Invalid URI! Must be an https URL.")
+end
+
+-- --- NETWORK CLOCK ---
 local globalTick = 0
 local function clock()
     while true do
         globalTick = globalTick + 1
-        rednet.broadcast({
-            type = "clock",
-            tick = globalTick
-        }, PROTOCOL)
+        rednet.broadcast({ type = "clock", tick = globalTick }, PROTOCOL)
         sleep(0)
     end
 end
 
+-- --- SEND AUDIO ---
 local function sendAudio()
     while true do
         local res = http.get(uri, nil, true)
