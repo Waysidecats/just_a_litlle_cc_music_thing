f1f64f1c181005a623536836ea732ea999ab1512
Waysidecats <103798521+Waysidecats@users.noreply.github.com>
2025-11-18 15:45:27 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,334 +1,197 @@
--- ==========================
--- Libraries
--- ==========================
-local basalt = require("lib/basalt")
-if not basalt then error("Basalt wasn't found") end
+local dfpwm = require("cc.audio.dfpwm")
+local speakersTemp = { peripheral.find("speaker") }
+local speakers = {}
+local drive = peripheral.find("drive")
+local decoder = dfpwm.make_decoder()
+local time = os.epoch("utc")
+local timeStorage = 0
+local menu = require "menu"
+local bigBuffer = {}
+local bigChunk
+local response
+local chunk;
+local uri = nil
+local volume = settings.get("media_center.volume")
+local selectedSong = nil
 
-local v = require("/lib/semver")
-if not v then error("semver wasn't found") end
-
-local dfpwm = require("/lib/dfpwm")
-if not dfpwm then error("dfpwm wasn't found") end
-
--- ==========================
--- Settings
--- ==========================
-local autoUpdates = settings.get("autoUpdates", true)
-local bufferLength = settings.get("bufferLength", 16)
-local clientVolume = settings.get("clientVolume", 1)
-local serverVolume = settings.get("serverVolume", 0)
-
--- Channels
-local controlChannel = settings.get("controlChannel", 2561)
-local bufferChannel = settings.get("bufferChannel", controlChannel + 1)
-local clientChannel = settings.get("clientChannel", controlChannel + 2)
-
--- Peripherals
-local modem = peripheral.find("modem")
-local speaker = peripheral.find("speaker")
-local monitor = peripheral.find("monitor")
-
-if not modem then error("There needs to be a modem attached") end
-if not speaker then error("There needs to be a speaker attached") end
-
--- ==========================
--- Local Index Builder (from saved songs)
--- ==========================
-local function buildLocalIndex()
-    local songs = {}
-    if not fs.exists("songs") then
-        fs.makeDir("songs")
-    end
-
-    for _, fileName in ipairs(fs.list("songs")) do
-        if string.sub(fileName, -4) == ".txt" then
-            local f = fs.open("songs/" .. fileName, "r")
-            local url = f.readAll()
-            f.close()
-
-            table.insert(songs, {
-                type = "song",
-                name = string.sub(fileName, 1, -5), -- remove ".txt"
-                author = "", -- author always empty
-                file = url
-            })
-        end
-    end
-
-    -- Always add an empty placeholder for new songs
-    table.insert(songs, { type="song", name="", author="", file="" })
-
-    return {
-        latestVersion = "1.0.0",
-        indexName = "Local Library",
-        songs = songs
-    }
+for i = 1, 10 do
+    speakers[i] = {}
 end
 
-local index = buildLocalIndex()
-
--- ==========================
--- musicme core
--- ==========================
-local musicme = {}
-local args = { ... }
-
--- Table search helper
-local tableFind = function(tbl, value)
-    local s = {}
-    for _, v in pairs(tbl) do s[v] = true end
-    return s[value]
+for i = 1, 20 do
+    bigBuffer[i] = ""
 end
 
--- Await message on channels
-local awaitMessage = function(channel, replyChannel, command)
-    local e, s, c, rc, msg, d = os.pullEvent("modem_message")
-    if command == "any" then
-        while c ~= channel and rc ~= replyChannel do
-            e, s, c, rc, msg, d = os.pullEvent("modem_message")
-        end
-        return msg
-    end
-    if command and command ~= "any" then
-        while c ~= channel and rc ~= replyChannel and msg.command ~= command do
-            e, s, c, rc, msg, d = os.pullEvent("modem_message")
-        end
-        return msg
-    end
-    error("Invalid arguments to awaitMessage")
+for i = 1, #speakerstemp do
+    local t = ((i - 1) % 10) + 1
+    table.insert(speakers[t], speakerstemp[i])
 end
 
--- Play dfpwm buffer
-local playBuffer = function(buffer, volume)
-    if not volume then volume = 1 end
-    while not speaker.playAudio(buffer, volume) do os.pullEvent("speaker_audio_empty") end
-end
+if drive == nil or not drive.isDiskPresent() then
+	local savedSongs = fs.list("songs/")
 
--- ==========================
--- Client
--- ==========================
-musicme.client = function(arguments)
-    modem.open(bufferChannel)
-    modem.open(clientChannel)
+	if #savedSongs == 0 then
+		error("ERR - No disk was found in the drive, or no drive was found. No sound files were found saved to device.")
+	else
+		local entries = {
+			[1] = {
+				label = "[CANCEL]",
+				callback = function()
+					error()
+				end
+			}
+		}
 
-    local bufferPlayback = function()
-        local msg
-        while true do
-            msg = awaitMessage(bufferChannel, controlChannel, "buffer")
-            if msg.buffer then playBuffer(msg.buffer, clientVolume) end
-        end
-    end
+		for i, fp in ipairs(savedSongs) do
+			table.insert(entries, {
+				label = fp:match("^([^.]+)"),
+				callback = function()
+					selectedSong = fp
 
-    local receiveMessage = function()
-        local msg
-        while true do
-            msg = awaitMessage(clientChannel, controlChannel, "any")
-            if msg.command == "start" then shell.run("reboot") end
-            if msg.command == "pause" then
-                if msg.pause then speaker.stop() end
-                shell.run("reboot")
-            end
-            if msg.command == "stop" then
-                speaker.stop()
-                shell.run("reboot")
-            end
-            if msg.command == "volume" then
-                clientVolume = msg.volume
-            end
-        end
-    end
+					menu.exit()
+				end
+			})
+		end
 
-    parallel.waitForAll(bufferPlayback, receiveMessage)
-end
+		menu.init({
+			main = {
+				entries = entries
+			}
+		})
 
--- ==========================
--- Get song handle
--- ==========================
-local getSongHandle = function(songID)
-    if not songID then error("songID is nil") end
-    local h, err = http.get({["url"] = songID.file, ["binary"] = true, ["redirect"] = true})
-    if not h then error("Failed to open song file: " .. err) end
-    return h
-end
+		menu.thread()
 
--- ==========================
--- GUI Server
--- ==========================
-musicme.gui = function(arguments)
-    if arguments[2] and tonumber(arguments[2]) then
-        serverVolume = math.min(math.max(tonumber(arguments[2]), 0), 3)
-    end
+		if selectedSong ~= nil then
+			local fp = "songs/" .. selectedSong
 
-    modem.open(controlChannel)
-    modem.open(clientChannel)
+			if fs.exists(fp) then
+				local file = fs.open(fp, "r")
 
-    local main = basalt.createFrame()
-    if not main then error("Failed to create basalt frame") end
+				uri = file.readAll()
 
-    local playbackThread = main:addThread()
-    local decoder = dfpwm.make_intdecoder()
+				file.close()
+			else
+				print("Song was not found on device!")
 
-    local pause = false
-    local playback = false
-    local shuffle = false
-    local selectedSong = nil
-    local playingSong = nil
-
-    local list = main:addList()
-        :setPosition(2, 2)
-        :setSize("parent.w - 2", "parent.h - 6")
-
-    for i, o in pairs(index.songs) do
-        list:addItem(index.songs[i].name)
-    end
-
-    main:onClick(function() selectedSong = index.songs[list:getItemIndex()] end)
-
-    local currentlyPlaying = main:addLabel()
-        :setPosition(29, "parent.h - 3")
-        :setSize("parent.w - 42", 3)
-        :setText("Now Playing: ")
-
-    local updateTrack = function()
-        if playingSong and playback then
-            local status = pause and "Paused: " or "Now Playing: "
-            currentlyPlaying:setText(status .. playingSong.name)
-        else
-            currentlyPlaying:setText("Now Playing: ")
-        end
-    end
-    main:onEvent(updateTrack)
-
-    local setVolume = function()
-        modem.transmit(clientChannel, controlChannel, {command="volume", volume=clientVolume})
-    end
-
-    local pausePlayback = function()
-        pause = not pause
-        modem.transmit(clientChannel, controlChannel, {command="pause", pause=pause})
-    end
-
-    local stopPlayback = function()
-        modem.transmit(clientChannel, controlChannel, {command="stop", stop=true})
-        playback = false
-        playbackThread:stop()
-    end
-
-    local broadcastSong = function(song)
-        if not song or song.file == "" then return end
-        playingSong = song
-        local songHandle = getSongHandle(song)
-        while true do
-            while pause do os.pullEvent() end
-            local chunk = songHandle.read(128 * bufferLength)
-            if not chunk then break end
-            local buffer = decoder(chunk)
-            modem.transmit(bufferChannel, controlChannel, {command="buffer", buffer=buffer})
-            playBuffer(buffer, serverVolume)
-        end
-        songHandle.close()
-    end
-
-    local startPlayback = function()
-        playback = true
-        setVolume()
-        modem.transmit(clientChannel, controlChannel, {command="start", start=true})
-
-        local broadcast = function()
-            if not shuffle then broadcastSong(selectedSong) end
-            if shuffle then
-                local history = {}
-                while shuffle do
-                    if #history > math.floor(#(index.songs)/2) then table.remove(history) end
-                    local rand = math.random(1, #(index.songs))
-                    while tableFind(history, rand) do rand = math.random(1, #(index.songs)) end
-                    table.insert(history, 1, rand)
-                    list:selectItem(rand)
-                    selectedSong = index.songs[rand]
-                    broadcastSong(selectedSong)
-                end
-            end
-        end
-
-        playbackThread:start(broadcast)
-    end
-
-    -- Buttons
-    local playButton = main:addButton():setPosition(2, "parent.h - 3"):setSize(6,3):setText("Play"):setBackground(colors.lime)
-    local pauseButton = main:addButton():setPosition(10,"parent.h-3"):setSize(9,3):setText("Pause"):setBackground(colors.orange)
-    local stopButton = main:addButton():setPosition(21,"parent.h-3"):setSize(6,3):setText("Stop"):setBackground(colors.red)
-    local shuffleButton = main:addButton():setPosition("parent.w-9","parent.h-2"):setSize(4,1):setText("=>"):setBackground(colors.red)
-    local volumeUpButton = main:addButton():setPosition("parent.w-3","parent.h-3"):setSize(3,1):setText("+")
-    local volumeDownButton = main:addButton():setPosition("parent.w-3","parent.h-1"):setSize(3,1):setText("-")
-
-    playButton:onClick(function() startPlayback(); updateTrack() end)
-    pauseButton:onClick(function()
-        pausePlayback()
-        if pause then pauseButton:setText("Unpause"):setBackground(colors.green)
-        else pauseButton:setText("Pause"):setBackground(colors.orange) end
-        updateTrack()
-    end)
-    stopButton:onClick(function()
-        stopPlayback()
-        pause = false
-        pauseButton:setText("Pause"):setBackground(colors.orange)
-        selectedSong = nil
-        updateTrack()
-    end)
-    volumeUpButton:onClick(function() clientVolume = math.min(clientVolume+0.1,3); setVolume() end)
-    volumeDownButton:onClick(function() clientVolume = math.max(clientVolume-0.1,0); setVolume() end)
-    shuffleButton:onClick(function()
-        shuffle = not shuffle
-        shuffleButton:setBackground(shuffle and colors.green or colors.red)
-    end)
-
-    basalt.autoUpdate()
-end
-
--- ==========================
--- Help
--- ==========================
-musicme.help = function(arguments)
-    print([[
-All computers running musicme must have a modem and speaker attached.
-The GUI server computer defaults to being muted.
-Currently the clients will reboot whenever the pause or stop buttons are hit.
-Configuring auto startup is highly encouraged using 'musicme startup'.
-
-Usage: <action> [arguments]
-Actions:
-musicme
-    help                -- Displays this message
-    gui <serverVolume>  -- Starts the GUI. Will automatically detect monitors.
-    client              -- Runs the client.
-    startup <arg>       -- Creates a startup file. Specify whether it is for 'client' or for 'gui'
-]])
-end
-
--- ==========================
--- Startup
--- ==========================
-musicme.startup = function(arguments)
-    local mode = table.remove(arguments,1)
-    if mode ~= "client" and mode ~= "gui" then
-        print("Must indicate whether startup file is for GUI or client")
-        return
-    end
-    if fs.exists("startup.lua") then fs.move("startup.lua","/old.musicme/startup.lua") end
-    if mode == "client" then fs.copy("/lib/clientStartup.lua","startup.lua") end
-    if mode == "gui" then fs.copy("/lib/guiStartup.lua","startup.lua") end
-    print("startup.lua created successfully")
-end
-
--- ==========================
--- Execute command
--- ==========================
-shell.run("clear")
-local command = table.remove(args,1)
-if monitor and command=="gui" and args[1]~="monitor" then
-    musicme.monitor(args)
-elseif musicme[command] then
-    musicme[command](args)
+				return
+			end
+		else error() end
+	end
 else
-    print("Please provide a valid command. For usage, use `musicme help`.")
+	local songFile = fs.open("disk/song.txt", "r")
+	uri = songFile.readAll()
+
+	songFile.close()
 end
+
+if uri == nil or not uri:find("^https") then
+	print("ERR - Invalid URI!")
+	return
+end
+
+function playChunk(chunk, batch)
+	local returnValue = nil
+	local callbacks = {}
+
+	for i, speaker in pairs(speakers[batch]) do
+		if i > 1 then
+			table.insert(callbacks, function()
+				speaker.playAudio(chunk, volume or 1.0)
+			end)
+		else
+			table.insert(callbacks, function()
+				returnValue = speaker.playAudio(chunk, volume or 1.0)
+			end)
+		end
+	end
+
+	parallel.waitForAll(table.unpack(callbacks))
+
+	return returnValue
+end
+
+print("Playing '" .. "' at volume " .. (volume or 1.0))
+
+local quit = false
+
+function play()
+	while true do
+    if timeStorage ~= math.floor(time) then
+        timeStorage = math.floor(time)
+        if bigChunk ~= nil then
+            playChunk(bigChunk, timeStorage%10+1)
+        end
+        getChunk()
+        local t = {}
+        for i = 1, 10 do
+            t[i] = bigBuffer[i] or ""
+        end
+        bigChunk = table.concat(t)
+	end
+end
+
+function readUserInput()
+	local commands = {
+		["stop"] = function()
+			quit = true
+		end
+	}
+
+	while true do
+		local input = string.lower(read())
+		local commandName = ""
+		local cmdargs = {}
+
+		local i = 1
+		for word in input:gmatch("%w+") do
+			if i > 1 then
+				table.insert(cmdargs, word)
+			else
+				commandName = word
+			end
+		end
+
+		local command = commands[commandName]
+
+		if command ~= nil then
+			command(table.unpack(cmdargs))
+		else print('"' .. cmdargs[1] .. '" is not a valid command!') end
+	end
+end
+
+function waitForQuit()
+	while not quit do
+		sleep(0.1)
+	end
+end
+
+function getChunk() 
+    if response == nil then
+        response = http.get(uri, nil, true)
+    end
+
+	local chunkSize = 4800
+    if chunk == nil then
+	    chunk = response.read(chunkSize)
+    end
+	local buffer = decoder(chunk)
+    for i = 20, 2, -1 do
+        bigBuffer[i] = bigBuffer[i-1]
+    end
+    bigBuffer[20] = buffer
+	chunk = response.read(chunkSize)
+
+    if chunk == nil then
+        response.close()
+        response = http.get(uri, nil, true)
+        chunk = response.read(chunkSize)
+    end
+end
+
+function timekeep()
+    while true do
+        time = os.epoch("utc")/100
+        sleep(0.01)
+    end
+end
+parallel.waitForAny(play, readUserInput, waitForQuit, timekeep)
